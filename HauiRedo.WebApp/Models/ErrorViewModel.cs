e2a38c02939f3255c3f464724b87bce60bbae332
Sweet <sweetlighthousevn@gmail.com>
2025-03-05 09:00:11 +0700

    Add default WebApp

---
+++ HauiRedo.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace HauiRedo.WebApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
